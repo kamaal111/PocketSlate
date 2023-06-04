89f373e320737ceb70695583bf6ac56551dac2bb
Kamaal <kamaal.f1@gmail.com>
2023-06-04 13:37:25 +0200

    Adding keyboard shortcuts to locale selector sheet

---
+++ PocketSlate/Extensions/EventModifiers+extensions.swift
@@ -0,0 +1,10 @@
+//
+//  EventModifiers+extensions.swift
+//  PocketSlate
+//
+//  Created by Kamaal M Farah on 04/06/2023.
+//
+
+import SwiftUI
+
+extension EventModifiers: Hashable { }
--- PocketSlate/Views/Sheets/LocaleSelectorSheet.swift
+++ PocketSlate/Views/Sheets/LocaleSelectorSheet.swift
@@ -8,43 +8,63 @@
 import SwiftUI
 import KamaalUI
 import AppLocales
+import KamaalLogger
 import KamaalAlgorithms
 import KamaalExtensions
+
+private let shortcuts: [KeyboardShortcutEvents: KeyboardShortcutConfiguration] = [
+    .downArrow: .init(id: UUID(uuidString: "b1227523-fa31-4aa5-be4f-c8dfad11d2df")!, key: .downArrow),
+    .upArrow: .init(id: UUID(uuidString: "1c63cd93-9bbd-43a9-bf59-89bfbaeaead7")!, key: .upArrow),
+    .return: .init(id: UUID(uuidString: "d969527a-bd58-49d9-ae43-92db3fd63b13")!, key: .return),
+].merged(with: (1 ..< 10).reduce([:]) { result, number in
+    let shortcut = KeyboardShortcutConfiguration(
+        id: UUID(),
+        key: KeyEquivalent("\(number)".first!),
+        modifiers: .command
+    )
+    return result.merged(with: [.number(value: number): shortcut])
+})
+
+private let logger = KamaalLogger(from: LocaleSelectorSheet.self)
 
 struct LocaleSelectorSheet: View {
     @EnvironmentObject private var userData: UserData
 
     @State private var searchTerm = ""
+    @State private var highlightedItem = 0
 
     let locales: [Locale]
     let onClose: () -> Void
     let onLocaleSelect: (_ locale: Locale) -> Void
 
     var body: some View {
-        KSheetStack(
-            title: AppLocales.getText(.LANGUAGES),
-            leadingNavigationButton: { leadingNavigationButton },
-            trailingNavigationButton: { Text.empty() }
-        ) {
-            VStack {
-                KFloatingTextField(text: $searchTerm, title: AppLocales.getText(.SEARCH))
-                ScrollView {
-                    ForEach(filteredLocales, id: \.identifier) { locale in
-                        localeButton(locale)
+        KeyboardShortcutView(shortcuts: shortcuts.values.asArray(), onEmit: handleKeyboardShortcut) {
+            KSheetStack(
+                title: AppLocales.getText(.LANGUAGES),
+                leadingNavigationButton: { leadingNavigationButton },
+                trailingNavigationButton: { Text.empty() }
+            ) {
+                VStack {
+                    KFloatingTextField(text: $searchTerm, title: AppLocales.getText(.SEARCH))
+                    ScrollView {
+                        ForEach(numberedFilteredLocales) { numberedLocale in
+                            localeButton(numberedLocale)
+                        }
+                        if filteredLocales.isEmpty {
+                            Text(localized: .NO_LOCALE_SEARCH_MATCH, with: [searchTerm])
+                            clearSearchButton
+                        }
                     }
-                    if filteredLocales.isEmpty {
-                        Text(localized: .NO_LOCALE_SEARCH_MATCH, with: [searchTerm])
-                        clearSearchButton
-                    }
+                    .ktakeWidthEagerly()
                 }
-                .ktakeWidthEagerly()
+                .padding(.vertical, .medium)
             }
-            .padding(.vertical, .medium)
+            .backgroundColor(light: .secondaryBackground.light, dark: .secondaryBackground.dark)
+            .onChange(of: searchTerm, perform: onSearchTermChange)
+            #if os(macOS)
+                .frame(minWidth: 300, minHeight: 200, maxHeight: 400)
+            #endif
         }
-        .backgroundColor(light: .secondaryBackground.light, dark: .secondaryBackground.dark)
-        #if os(macOS)
-            .frame(minWidth: 300, minHeight: 200, maxHeight: 400)
-        #endif
     }
 
     private var leadingNavigationButton: some View {
@@ -69,18 +89,45 @@ struct LocaleSelectorSheet: View {
         .buttonStyle(.plain)
     }
 
-    private func localeButton(_ locale: Locale) -> some View {
-        Button(action: { onLocaleSelect(locale) }) {
-            Text(makeMessage(from: locale))
-                .foregroundColor(.accentColor)
-                .bold()
-                .padding(.vertical, .extraExtraSmall)
-                .padding(.horizontal, .small)
-                .ktakeWidthEagerly(alignment: .leading)
-                .backgroundColor(light: .secondaryItemBackground.light, dark: .secondaryItemBackground.dark)
-                .cornerRadius(.extraSmall)
+    private func localeButton(_ numberedLocale: NumberedLocale) -> some View {
+        let button = Button(action: { onLocaleSelect(numberedLocale.locale) }) {
+            HStack {
+                Text(numberedLocale.message(appLocale: userData.appLocale))
+                    .foregroundColor(.accentColor)
+                    .bold()
+                    .ktakeWidthEagerly(alignment: .leading)
+                if numberedLocale.number < 9 {
+                    Spacer()
+                    Text("􀆔\(numberedLocale.number + 1)")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(.vertical, .extraExtraSmall)
+            .padding(.horizontal, .small)
+            .ktakeWidthEagerly()
+            .backgroundColor(light: .secondaryItemBackground.light, dark: .secondaryItemBackground.dark)
+            .cornerRadius(.extraSmall)
         }
         .buttonStyle(.plain)
+
+        return KJustStack {
+            if highlightedItem == numberedLocale.number {
+                button
+                    .padding(.all, .extraExtraSmall)
+                    .overlay(RoundedRectangle(cornerRadius: AppSizes.small.rawValue)
+                        .inset(by: 2)
+                        .stroke(Color.accentColor.opacity(0.5), lineWidth: 2)
+                        .clipped())
+            } else {
+                button
+            }
+        }
+    }
+
+    private var numberedFilteredLocales: [NumberedLocale] {
+        filteredLocales
+            .enumerated()
+            .map { NumberedLocale(locale: $0.element, number: $0.offset) }
     }
 
     private var filteredLocales: [Locale] {
@@ -97,12 +144,63 @@ struct LocaleSelectorSheet: View {
             }
     }
 
+    private func handleKeyboardShortcut(_ shortcut: KeyboardShortcutConfiguration) {
+        guard let event = shortcuts.find(by: \.value.id, is: shortcut.id)?.key else {
+            logger.error("Failed to find shortcut event")
+            return
+        }
+
+        switch event {
+        case .downArrow:
+            if (highlightedItem + 1) < filteredLocales.count {
+                highlightedItem += 1
+            }
+        case .upArrow:
+            if highlightedItem != 0 {
+                highlightedItem -= 1
+            }
+        case .return:
+            if !filteredLocales.isEmpty {
+                onLocaleSelect(filteredLocales[highlightedItem])
+                clearSearchTerm()
+            }
+        case let .number(value):
+            assert([1, 2, 3, 4, 5, 6, 7, 8, 9].contains(value))
+            let isInBound = (1 ..< 10).contains(value)
+            assert(isInBound)
+            if isInBound {
+                onLocaleSelect(filteredLocales[value - 1])
+                clearSearchTerm()
+            }
+        }
+    }
+
+    private func onSearchTermChange(_: String) {
+        if highlightedItem != 0 {
+            withAnimation { highlightedItem = 0 }
+        }
+    }
+
     private func clearSearchTerm() {
         searchTerm = ""
     }
+}
 
-    private func makeMessage(from locale: Locale) -> String {
-        "\(locale.identifier) - \(userData.appLocale.localizedString(forIdentifier: locale.identifier)!)"
+private enum KeyboardShortcutEvents: Hashable {
+    case downArrow
+    case upArrow
+    case `return`
+    case number(value: Int)
+}
+
+private struct NumberedLocale: Hashable, Identifiable {
+    let locale: Locale
+    let number: Int
+
+    var id: String { locale.identifier }
+
+    func message(appLocale: Locale) -> String {
+        "\(locale.identifier) - \(appLocale.localizedString(forIdentifier: locale.identifier)!)"
     }
 }
 
