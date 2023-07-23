1ffa91d3d3772ba17c89f5084c835362116d517a
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-23 22:10:17 +0200

    Importing signing certificate

--- Scripts/encode_certificates.py
+++ Scripts/encode_certificates.py
@@ -1,50 +1,40 @@
 import base64
 import json
-import os
 from pathlib import Path
-from typing import Optional, TypedDict
 
-
-SECRETS_DIRECTORY = "Secrets"
 
 MAP_FILENAME_TO_CERTIFICATES = {
     "LexiGlotty_iOS_App_Store_distribution.mobileprovision": "ios_provisioning_profile",
-    "IOSCertificates.p12": "ios_signing_certificate",
+    "Certificates.p12": "signing_certificate",
 }
 
 
-class Certificates(TypedDict, total=False):
-    provisioning_profile: Optional[str]
-    signing_certificate: Optional[str]
-
-
-# def write_certificates_json(*, secrets_directory: Path, certificates_data: Certificates):
-#     certificates_json = json.dumps(certificates_data, indent=2)
-#     certificates_json_filepath = os.path.join(SECRETS_DIRECTORY, "certificates.json")
-
-#     with open(certificates_json_filepath, "w") as file:
-#         file.write(certificates_json)
+def write_certificates_json(
+    *, secrets_directory: Path, certificates_data: dict[str, str]
+):
+    certificates_json = secrets_directory / "certificates.json"
+    certificates_json.write_text(json.dumps(certificates_data, indent=2))
 
 
 def make_certificates_dict(*, secrets_directory: Path):
-    certificates: Certificates = {}
+    certificates: dict[str, str] = {}
 
     for path in secrets_directory.iterdir():
-        print(path)
-    # for filename in os.listdir(SECRETS_DIRECTORY):
-    #     if key := MAP_FILENAME_TO_CERTIFICATES.get(filename):
-    #         certificates[key] = encode_certificate(filename=filename)
+        if path.is_file() and (key := MAP_FILENAME_TO_CERTIFICATES.get(path.name)):
+            certificates[key] = encode_certificate(file=path)
 
     return certificates
 
 
-# def encode_certificate(*, filename: str):
-#     file = Path(SECRETS_DIRECTORY) / filename
-#     return base64.b64encode(file.read()).decode("utf-8")
+def encode_certificate(*, file: Path):
+    file_bytes = file.read_bytes()
+    return base64.b64encode(file_bytes).decode("utf-8")
 
 
 if __name__ == "__main__":
     secrets_directory = Path("Secrets")
     certificates_data = make_certificates_dict(secrets_directory=secrets_directory)
-    # write_certificates_json(certificates_data=certificates_data)
+    write_certificates_json(
+        secrets_directory=secrets_directory, certificates_data=certificates_data
+    )
     print("done encoding certificates ✨✨✨")
