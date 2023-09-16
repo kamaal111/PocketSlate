f4f79400bb24adaaa5ff39ffe84998ef9d3b9ab3
Kamaal Farah <kamaal.f1@gmail.com>
2023-09-16 23:10:13 +0200

    Adding new phrase panel

--- Modules/Features/Sources/PhrasesV2/PhrasesScreen.swift
+++ Modules/Features/Sources/PhrasesV2/PhrasesScreen.swift
@@ -5,7 +5,9 @@
 //  Created by Kamaal M Farah on 16/09/2023.
 //
 
+import AppUI
 import SwiftUI
+import KamaalUI
 
 public struct PhrasesScreen: View {
     @State private var viewModel = ViewModel()
@@ -20,8 +22,27 @@ public struct PhrasesScreen: View {
                 swapLocales: { viewModel.swapLocales() },
                 selectLocaleSelector: { localeSelector in viewModel.selectLocaleSelector(localeSelector) }
             )
+            KScrollableForm {
+                KSection(header: NSLocalizedString("New translation", bundle: .module, comment: "")) {
+                    NewPhrasePanel(
+                        primaryPhrase: $viewModel.newPrimaryPhrase,
+                        secondaryPhrase: $viewModel.newSecondaryPhrase,
+                        locales: viewModel.locales,
+                        submitNewPhrase: { submitNewPhrase() }
+                    )
+                }
+                #if os(macOS)
+                .padding(.horizontal, .small)
+                #endif
+            }
         }
+        .toolbar(content: {
+            EditButton()
+        })
+        .environment(\.editMode, $viewModel.editMode)
     }
+
+    private func submitNewPhrase() { }
 }
 
 #if DEBUG
